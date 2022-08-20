6a97f051bee28e947e000ad8769014c2da28104e
ManikandanBG <maniapn94@gmail.com>
2022-08-20 17:54:14 +0530

    Cart page react initialisation

---
+++ themes/html-templates/oganik/assets/js/meridaCustomised/cartPage.js
@@ -0,0 +1,255 @@
+class CartPageContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return React.createElement(
+      React.Fragment,
+      null,
+      React.createElement(
+        "div",
+        { className: "table-responsive" },
+        React.createElement(
+          "table",
+          { className: "table cart-table" },
+          React.createElement(
+            "thead",
+            null,
+            React.createElement(
+              "tr",
+              null,
+              React.createElement(
+                "th",
+                null,
+                "Item"
+              ),
+              React.createElement(
+                "th",
+                null,
+                "Price"
+              ),
+              React.createElement(
+                "th",
+                null,
+                "Quantity"
+              ),
+              React.createElement(
+                "th",
+                null,
+                "Total"
+              ),
+              React.createElement(
+                "th",
+                null,
+                "Remove"
+              )
+            )
+          ),
+          React.createElement(
+            "tbody",
+            null,
+            React.createElement(
+              "tr",
+              null,
+              React.createElement(
+                "td",
+                null,
+                React.createElement(
+                  "div",
+                  { className: "product-box" },
+                  React.createElement("img", { src: "assets/images/products/cart-1-1.jpg", alt: "" }),
+                  React.createElement(
+                    "h3",
+                    null,
+                    React.createElement(
+                      "a",
+                      { href: "product-details.html" },
+                      "Banana"
+                    )
+                  )
+                )
+              ),
+              React.createElement(
+                "td",
+                null,
+                "$9.99"
+              ),
+              React.createElement(
+                "td",
+                null,
+                React.createElement(
+                  "div",
+                  { className: "quantity-box" },
+                  React.createElement(
+                    "button",
+                    { type: "button", className: "sub" },
+                    "-"
+                  ),
+                  React.createElement("input", { type: "number", id: "2", value: "1" }),
+                  React.createElement(
+                    "button",
+                    { type: "button", className: "add" },
+                    "+"
+                  )
+                )
+              ),
+              React.createElement(
+                "td",
+                null,
+                "$9.99"
+              ),
+              React.createElement(
+                "td",
+                null,
+                React.createElement("i", { className: "organik-icon-close remove-icon" })
+              )
+            ),
+            React.createElement(
+              "tr",
+              null,
+              React.createElement(
+                "td",
+                null,
+                React.createElement(
+                  "div",
+                  { className: "product-box" },
+                  React.createElement("img", { src: "assets/images/products/cart-1-2.jpg", alt: "" }),
+                  React.createElement(
+                    "h3",
+                    null,
+                    React.createElement(
+                      "a",
+                      { href: "product-details.html" },
+                      "Tomatoes"
+                    )
+                  )
+                )
+              ),
+              React.createElement(
+                "td",
+                null,
+                "$9.99"
+              ),
+              React.createElement(
+                "td",
+                null,
+                React.createElement(
+                  "div",
+                  { className: "quantity-box" },
+                  React.createElement(
+                    "button",
+                    { type: "button", className: "sub" },
+                    "-"
+                  ),
+                  React.createElement("input", { type: "number", id: "2", value: "1" }),
+                  React.createElement(
+                    "button",
+                    { type: "button", className: "add" },
+                    "+"
+                  )
+                )
+              ),
+              React.createElement(
+                "td",
+                null,
+                "$9.99"
+              ),
+              React.createElement(
+                "td",
+                null,
+                React.createElement("i", { className: "organik-icon-close" })
+              )
+            )
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "row" },
+        React.createElement(
+          "div",
+          { className: "col-lg-8" },
+          React.createElement(
+            "form",
+            { action: "#", className: "contact-one__form" },
+            React.createElement("input", { type: "text", placeholder: "Enter Coupon Code" }),
+            React.createElement(
+              "button",
+              { type: "submit", className: "thm-btn" },
+              "Apply Coupon"
+            )
+          )
+        ),
+        React.createElement(
+          "div",
+          { className: "col-lg-4" },
+          React.createElement(
+            "ul",
+            { className: "cart-total list-unstyled" },
+            React.createElement(
+              "li",
+              null,
+              React.createElement(
+                "span",
+                null,
+                "Subtotal"
+              ),
+              React.createElement(
+                "span",
+                null,
+                "$19.98 USD"
+              )
+            ),
+            React.createElement(
+              "li",
+              null,
+              React.createElement(
+                "span",
+                null,
+                "Shipping Cost"
+              ),
+              React.createElement(
+                "span",
+                null,
+                "$0.00 USD"
+              )
+            ),
+            React.createElement(
+              "li",
+              null,
+              React.createElement(
+                "span",
+                null,
+                "Total"
+              ),
+              React.createElement(
+                "span",
+                null,
+                "$19.98 USD"
+              )
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "button-box" },
+            React.createElement(
+              "a",
+              { href: "#", className: "thm-btn" },
+              "Update"
+            ),
+            React.createElement(
+              "a",
+              { href: "#", className: "thm-btn" },
+              "Checkout"
+            )
+          )
+        )
+      )
+    );
+  }
+}
+
+ReactDOM.createRoot(document.querySelector("section.cart-page > div.container")).render(React.createElement(CartPageContainer, null));
---
+++ themes/html-templates/oganik/assets/js/meridaCustomised/cartPage.jsx
@@ -0,0 +1,118 @@
+class CartPageContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="table-responsive">
+          <table className="table cart-table">
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="product-box">
+                    <img src="assets/images/products/cart-1-1.jpg" alt="" />
+                    <h3>
+                      <a href="product-details.html">Banana</a>
+                    </h3>
+                  </div>
+                </td>
+                <td>$9.99</td>
+                <td>
+                  <div className="quantity-box">
+                    <button type="button" className="sub">
+                      -
+                    </button>
+                    <input type="number" id="2" value="1" />
+                    <button type="button" className="add">
+                      +
+                    </button>
+                  </div>
+                </td>
+                <td>$9.99</td>
+                <td>
+                  <i className="organik-icon-close remove-icon"></i>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="product-box">
+                    <img src="assets/images/products/cart-1-2.jpg" alt="" />
+                    <h3>
+                      <a href="product-details.html">Tomatoes</a>
+                    </h3>
+                  </div>
+                </td>
+                <td>$9.99</td>
+                <td>
+                  <div className="quantity-box">
+                    <button type="button" className="sub">
+                      -
+                    </button>
+                    <input type="number" id="2" value="1" />
+                    <button type="button" className="add">
+                      +
+                    </button>
+                  </div>
+                </td>
+                <td>$9.99</td>
+                <td>
+                  <i className="organik-icon-close"></i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="row">
+          <div className="col-lg-8">
+            <form action="#" className="contact-one__form">
+              <input type="text" placeholder="Enter Coupon Code" />
+              <button type="submit" className="thm-btn">
+                Apply Coupon
+              </button>
+            </form>
+          </div>
+          <div className="col-lg-4">
+            <ul className="cart-total list-unstyled">
+              <li>
+                <span>Subtotal</span>
+                <span>$19.98 USD</span>
+              </li>
+              <li>
+                <span>Shipping Cost</span>
+                <span>$0.00 USD</span>
+              </li>
+              <li>
+                <span>Total</span>
+                <span>$19.98 USD</span>
+              </li>
+            </ul>
+            <div className="button-box">
+              <a href="#" className="thm-btn">
+                Update
+              </a>
+              <a href="#" className="thm-btn">
+                Checkout
+              </a>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.createRoot(document.querySelector("section.cart-page > div.container")).render(
+  <CartPageContainer />);
